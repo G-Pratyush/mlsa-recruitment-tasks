3269dd09a8c9c5aeaa4e7bb3bbdb99202cd24aa3
G-Pratyush <74899637+G-Pratyush@users.noreply.github.com>
2020-12-03 20:42:07 +0530

    first commit

--- task3_part2.cpp
+++ task3_part2.cpp
@@ -1,30 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b)                          
-{ 
-    if (a == b)
-        return a; 
-    if (a > b)
-    {
-        return gcd(a - b, b);
-    }
-    else
-    {
-        return gcd(a, b - a);
-    }        
-}
-
 int main()
 {
 	int a,b;
 	cin>>a>>b;
     if ((a == 0) || (b == 0))
+    {
         cout<<"0 not allowed!";
         return 1;
-    while (gcd(a,b)!=1) 
+    }
+    while (__gcd(a,b)!=1) 
     {
-        int d=gcd(a,b);
+        int d=__gcd(a,b);
         a=a/d;                          
     }                                         
     cout<<a;
